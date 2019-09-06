63119b9482d6b737f8abc917883a0ed16b5c18a3
João Gabriel <joao.gbp@hotmail.com>
2019-09-06 19:24:03 -0300

    Add Xcode Project

---
+++ App-Movies/App-Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App-Movies
+//
+//  Created by João Gabriel Borelli Padilha on 06/09/19.
+//  Copyright © 2019 João Gabriel Borelli Padilha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ App-Movies/App-MoviesTests/App_MoviesTests.swift
@@ -0,0 +1,34 @@
+//
+//  App_MoviesTests.swift
+//  App-MoviesTests
+//
+//  Created by João Gabriel Borelli Padilha on 06/09/19.
+//  Copyright © 2019 João Gabriel Borelli Padilha. All rights reserved.
+//
+
+import XCTest
+@testable import App_Movies
+
+class App_MoviesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
